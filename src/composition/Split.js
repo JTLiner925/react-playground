69c445482b4d903536c008401e058975cea3d692
JTLiner925 <[jtliner925@gmail.com]>
2020-03-26 10:13:05 -0500

    styles app.js, excplicitly add feature to custom components

--- src/composition/Split.js
+++ src/composition/Split.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { directive } from '@babel/types';
 import './Split.css';
+import { directive } from '@babel/types';
+
 //Inside Split.js we will import React so that we can use JSX.
 
 //when creating your component capitalize the first letter
 //when using props.children you need to put props in the arg
 function Split(props) {
+    const combinedClassName = `split ${props.className}`
     return (
         //remember className not class
         //if you want to put the text in the 
         //app file you must use {props.children}
-        <div className="split">
+        <div className={combinedClassName}>
       {props.children}
     </div>
     );
