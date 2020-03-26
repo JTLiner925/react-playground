079104ce2d89407ac8c094167ac709755e4cb434
JTLiner925 <[jtliner925@gmail.com]>
2020-03-26 09:15:45 -0500

    Split app, import into app.js and called

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Split from './composition/Split';
+//we import the split file/component to use 
+//in the app function
 
 function App() {
   return (
     <main className='App'>
-      {/* content goes here */}
+      <Split />
+      <Split />
+      <Split />
+      <Split />
     </main>
   );
 }
--- src/composition/Split.js
+++ src/composition/Split.js
@@ -1,1 +1,17 @@
 import React from 'react'
+import { directive } from '@babel/types'
+//Inside Split.js we will import React so that we can use JSX.
+
+//when creating your component capitalize the first letter
+function Split() {
+    return (
+        //remember className not class
+        <div className='split'>
+        This is the content of the split component. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+        </div>
+    );
+}
+
+//Now we need to export this component 
+//so that we can import it into the App. 
+export default Split;
