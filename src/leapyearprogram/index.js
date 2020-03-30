1302b2b8cab45aae26ce02ecc7ddfcc6f778c08b
JTLiner925 <[jtliner925@gmail.com]>
2020-03-30 11:10:37 -0500

    looking at app.test

--- src/leapyearprogram/index.js
+++ src/leapyearprogram/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 
 function leapYear(year) {
     if(year < 1582) {
@@ -7,7 +6,7 @@ function leapYear(year) {
     const div400 = year % 400 === 0;
     const div100 = year % 100 === 0;
     const div4 = year % 4 === 0;
-    return div400 || (div4 || !div100);
+    return div400 || (div4 && !div100);
   }
   
   export default leapYear;
