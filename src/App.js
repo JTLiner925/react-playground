32e7cbcfa77eb17bb68a5dcd4023bba4d9c3b53d
JTLiner925 <[jtliner925@gmail.com]>
2020-03-30 11:47:32 -0500

    added Messages.test.js and ran successful test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css'
+import Messages from "./Messages";
 
-function App() {
+class App extends Component {
+  render() {
   return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
+    <div className="App">
+      <h1>YOUR APPLICATION NAME!</h1>
+      <div>Hello!</div>
+      <Messages name="Messages" unread={0} />
+      <Messages name="Notifications" unread={10} />
+    </div>
   );
+}
 }
 
 export default App;
