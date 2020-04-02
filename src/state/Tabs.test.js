1ae23ec86ce89a6ca442c9f961c02a5fe8ca6d67
JTLiner925 <[jtliner925@gmail.com]>
2020-04-02 15:53:57 -0500

    using enzyme to see if buttons are working correctly

--- src/state/Tabs.test.js
+++ src/state/Tabs.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import renderer from "react-test-renderer";
+// import renderer from "react-test-renderer";
 import Tabs from "./Tabs";
 import { shallow } from "enzyme";
 import toJson from 'enzyme-to-json'
@@ -31,12 +31,20 @@ describe(`Tabs Component`, () => {
   });
 
   it("renders the first tab by default", () => {
-    const tree = renderer.create(<Tabs tabs={tabsProp} />).toJSON();
-    expect(tree).toMatchSnapshot();
+    // const tree = renderer.create(<Tabs tabs={tabsProp} />).toJSON();
+    // expect(tree).toMatchSnapshot();
+    const wrapper = shallow(<Tabs tabs={tabsProp} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
   });
 
   it("renders empty given no tabs", () => {
     const wrapper = shallow(<Tabs />)
     expect(toJson(wrapper)).toMatchSnapshot()
   });
+
+  it('closes the first tab and opens any clicked tab', () => {
+    const wrapper = shallow(<Tabs tabs={tabsProp} />)
+    wrapper.find('button').at(1).simulate('click')
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
 });
