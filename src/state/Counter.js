dd9fd596eddb157f534e22b8a131ae373dbed1a7
JTLiner925 <[jtliner925@gmail.com]>
2020-04-02 10:54:50 -0500

    finished lesson

--- src/state/Counter.js
+++ src/state/Counter.js
@@ -1,12 +1,16 @@
 import React from "react";
 class Counter extends React.Component {
     constructor(props){
+        console.log('props in constructor', props)
         super(props)
     this.state = {
       count: 0
     }
     };
+    
     handleButtonClick = () => {
+        console.log('props in handleButtonClick', this.props)
+    console.log('state in handleButtonClick', this.state)
         const newCount = this.state.count + 1
         this.setState({
             count: newCount
