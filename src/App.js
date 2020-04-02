20b3d5d1603a3ff513290ceaf3ca050970c85016
JTLiner925 <[jtliner925@gmail.com]>
2020-04-02 14:21:59 -0500

    render buttons and render first tab by default

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import './App.css'
-import Messages from "./Messages";
-import TheDate from './state/TheDate';
-import Counter from './state/Counter';
+import './App.css';
+import Tabs from './state/Tabs';
 
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
 class App extends Component {
   render() {
   return (
     <div className="App">
-      <h1>YOUR APPLICATION NAME!</h1>
-      <div>Hello!</div>
-      <Messages name="Messages" unread={0} />
-      <Messages name="Notifications" unread={10} />
-      <TheDate />
-      <Counter />
+      <Tabs tabs={tabsProp} />
     </div>
   );
 }
