4c3bc4ddf13d8cbcc32520ab359a080945b057db
JTLiner925 <[jtliner925@gmail.com]>
2020-04-02 11:29:23 -0500

    setting up Bomb.js

---
+++ src/state-drills/Bomb.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Bomb extends React.Component {
+    render(){
+        return(
+            <div>
+                <p>hi</p>
+            </div>
+        )
+    }
+}
+export default Bomb;
