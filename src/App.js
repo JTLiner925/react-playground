a5df0961621d051431a058f8e4d0a17f4a190025
JTLiner925 <[jtliner925@gmail.com]>
2020-04-02 18:25:49 -0500

    starting accordion assignment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Tabs from './state/Tabs';
+import Accordion from './state-drills/Accordion.js';
 
-const tabsProp = [
-  { name: 'First tab',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
-  { name: 'Second tab',
-    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
-  { name: 'Third tab',
-    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
-];
+
 class App extends Component {
   render() {
   return (
---
+++ src/state-drills/Accordion.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Accordion extends React.Component {
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Accordion;
