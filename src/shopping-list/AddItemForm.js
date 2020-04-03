59fc38aa1ba8fac9f897e39ec6ecf4125eaf17bf
JTLiner925 <[jtliner925@gmail.com]>
2020-04-03 13:39:59 -0500

    added AddItemForm class to file

---
+++ src/shopping-list/AddItemForm.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class AddItemForm extends React.Component {
+    render(){
+        return (
+            <form>
+              <input
+                type='text'
+                placeholder='carrots'
+                aria-label='Shopping list item'
+              />
+              <button type='submit'>Add Item</button>
+            </form>
+        )
+    }
+}
+export default AddItemForm;
