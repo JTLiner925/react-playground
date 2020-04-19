cced9105345b3d981a5d98fe31ba69d37159dc80
JTLiner925 <[jtliner925@gmail.com]>
2020-04-19 09:31:00 -0500

    Split component

---
+++ src/composition/Split.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './Split.css';
+
+export default function Split(props) {
+  const combinedClassName = `split ${props.className}`;
+  const newStyles = { 
+    flex: props.flexBasis, 
+    fontSize: props.fontSize 
+  };
+ 
+  return (
+    <div 
+      className={combinedClassName}
+      style={newStyles}
+      >{props.children}
+    </div>
+  );
+}
