dde31187b6bb429d118e95ce4bcfe0e7b5f0f97d
JTLiner925 <[jtliner925@gmail.com]>
2020-04-20 12:33:46 -0500

    updated app to render Tabs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import HelloWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun';
 import Accordion from './state-drills/Accordion';
+import Tabs from './state/Tabs';
 import './App.css'
+
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
 
 const sections = [
   {
@@ -24,6 +34,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <h2>Tabs</h2>
+        <Tabs tabs={tabsProp} />
         <h2>Hello World</h2>
         <HelloWorld />
         <h2>Bomb</h2>
