3b91be5f0499ecd1bfe470acbe96a532e7de848f
JTLiner925 <[jtliner925@gmail.com]>
2020-04-20 10:49:11 -0500

    tooltip

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from "react";
 import Split from "./composition/Split";
 import "./App.css";
+import Tooltip from "./composition/Tooltip";
+
+const firstTooltip = (
+  <Tooltip message="tooltip message" color="hotpink">
+    ipsum
+  </Tooltip>
+);
+const secondTooltip = (
+  <Tooltip message="another tooltip message" color="red">
+    officiis
+  </Tooltip>
+);
 function App() {
   return (
     <main className="App">
-      <Split className='left' flexBasis={2}>
-        This is the content for the left `Split`. Lorem ipsum dolor sit amet
-        consectetur, adipisicing elit. Incidunt ex velit suscipit facere
+      <Split className="left" flexBasis={2}>
+        This is the content for the left `Split`. Lorem {firstTooltip} dolor sit
+        amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere
         officia?
+        <br />
+        <Tooltip message="one more tooltip message">
+          Necessitatibus?
+        </Tooltip>
       </Split>
-      <Split className='right' fontSize={48}>
+      <Split className="right" fontSize={48}>
         This is the content for the right `Split`. Inventore aliquid cupiditate
-        suscipit repellat. Quaerat quis officiis quam fuga. Aliquid quo possimus
-        id soluta aspernatur.
+        suscipit repellat. Quaerat quis {secondTooltip} quam fuga. Aliquid quo
+        possimus id soluta aspernatur.
       </Split>
     </main>
   );
---
+++ src/composition/Tooltip.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Tooltip.css";
+
+// export default function Tooltip(props) {
+//   return (
+//     <span className="Tooltip">
+//       <span className="Tooltip-content" style={{ color: props.color }}>
+//         {props.children}
+//       </span>
+//       <div className="Tooltip-message">{props.message}</div>
+//     </span>
+//   );
+// }
+
+export default class TooltipClass extends React.Component{
+  static defaultProps = {
+    color: '#01A800', // green
+  };
+  render(){
+    console.log('Using a class component!')
+    console.log(this.props)
+    return(
+      <span className="Tooltip">
+      <span className="Tooltip-content" style={{ color: this.props.color }}>
+        {this.props.children}
+      </span>
+      <div className="Tooltip-message">{this.props.message}</div>
+    </span>
+    )
+  }
+}
