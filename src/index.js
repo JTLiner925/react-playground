99355179d2f3f304660063b60560c81ba5965749
JTLiner925 <[jtliner925@gmail.com]>
2020-05-04 11:50:26 -0500

    updated index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from './playingwithdanger/App';
+// import AppLang from './lang-context/AppLang';
+// import App from './playingwithdanger/App';
+import RegistrationForm from './registrationForm/registrationForm';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<RegistrationForm />, document.getElementById('root'));
+// ReactDOM.render(
+// <AppLang />, 
+// document.getElementById("root"));
